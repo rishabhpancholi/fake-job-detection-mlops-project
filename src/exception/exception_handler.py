f2323284efbf757807c3a237f1596fde6ba2e53a
Rishabh Pancholi <rp23@iitbbs.ac.in>
2025-09-28 21:06:56 +0530

    exception module added

---
+++ src/exception/exception_handler.py
@@ -0,0 +1,28 @@
+# Imports
+import sys
+
+class CustomException(Exception):
+    """Custom exception class for the project"""
+    def __init__(self, error_message: Exception, error_detail:sys):
+        super().__init__(str(error_message))
+        self.error_message = str(error_message)
+        self.error_detail = error_detail
+        _,_,exc_tb = self.error_detail.exc_info() # Extracts the traceback
+        self.line_number = exc_tb.tb_lineno # Extracts the line number
+        self.file_name = exc_tb.tb_frame.f_code.co_filename # Extracts the file name
+
+    def get_detailed_message(self)->str:
+        """Gets the detailed formatted error message"""
+        detailed_message = f"Error occurred in script [{self.file_name}] at line number [{self.line_number}] with error message [{self.error_message}]" # Creates the detailed message
+        return detailed_message
+    
+    def __str__(self)->str:
+        """Returns the formatted error message"""
+        return self.get_detailed_message() # Returns the detailed message
+    
+
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except ZeroDivisionError as e:
+        raise CustomException(e,sys)
